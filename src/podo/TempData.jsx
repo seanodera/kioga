d06ad18a9ea143e7b5dc5e68437884aad4b80a17
Sean Olero <seanolero@outlook.com>
2022-09-30 04:02:03 +0300

    temporary data increase

--- src/podo/TempData.jsx
+++ src/podo/TempData.jsx
@@ -1,5 +1,5 @@
 let works = [{
-    image: '/assets/image10.jpg',
+    image: '/assets/image1.jpg',
     name: 'Tunnel Somewhere',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id nibh ultrices, maximus diam et, hendrerit nisi. Suspendisse molestie dolor vitae dui feugiat, quis eleifend lectus fermentum. Donec ac lorem enim. Phasellus eget massa elit. Donec ut libero tempor, pretium mauris feugiat, aliquam dolor. Vestibulum vestibulum justo nec sem auctor rutrum. Integer tempus malesuada augue, et consectetur ex accumsan vitae. Vestibulum ut neque elit. Nunc pulvinar cursus felis, id pulvinar metus lacinia vel. Mauris iaculis sem eget metus efficitur, dignissim convallis orci malesuada. Maecenas finibus urna vel eros ullamcorper, ac ultricies tellus luctus. Etiam egestas enim felis, non laoreet turpis sollicitudin sit amet. Sed egestas turpis sit amet vehicula imperdiet. Pellentesque leo metus, interdum sit amet molestie non, commodo non purus.',
     id: 0,
@@ -47,26 +47,46 @@ let works = [{
     id: 8,
     type: 'remote location'
 }, {
-    image: '/assets/image7.jpg',
+    image: '/assets/image10.jpg',
     name: 'Apartment Complex',
     description: 'Vestibulum lectus dolor, pellentesque sed blandit sed, tempor eget odio. Maecenas luctus mauris justo, eget fringilla justo fermentum aliquet. Aliquam imperdiet dapibus tellus sit amet ultricies. Duis tincidunt nibh quis lectus dapibus interdum. Duis a viverra tortor. Ut laoreet, justo sit amet posuere tincidunt, ipsum enim dictum quam, quis pulvinar turpis lorem sed urna. Morbi eu mauris sit amet sem aliquam venenatis in ac sapien. Donec sed iaculis nibh. Vestibulum ultricies sapien eget sem placerat, eget porta lacus vehicula. Etiam venenatis massa dignissim velit iaculis, in elementum quam dapibus. Suspendisse semper vulputate elit et imperdiet.',
     id: 9,
     type: 'residentual buildings'
 }, {
-    image: '/assets/image8.jpg',
+    image: '/assets/image1.jpg',
     name: 'Port Of Somewhere',
     description: 'Integer egestas mi in arcu elementum, pharetra suscipit risus efficitur. Cras a vehicula dui, quis ultrices justo. Aliquam mattis sem euismod, mattis tellus nec, pretium lorem. Aliquam dignissim urna et auctor sollicitudin. Quisque facilisis placerat lectus eget sodales. Mauris condimentum tempus interdum. Quisque dolor nunc, bibendum ut diam ac, elementum tristique sapien. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi finibus eget nisi nec blandit. Suspendisse purus magna, ornare non magna in, feugiat iaculis justo. Ut non augue erat. Nullam lorem urna, posuere vitae neque quis, vestibulum congue magna. Mauris et tincidunt elit.',
     id: 10,
     type: 'Heavy Equipment Installation'
 }, {
-    image: '/assets/image9.jpg',
+    image: '/assets/image2.jpg',
     name: 'Power Lines',
     description: 'Maecenas feugiat nisl non pharetra tempor. Pellentesque porttitor pulvinar dui, nec euismod sem facilisis sed. Sed efficitur faucibus pulvinar. Sed eu tortor eget odio venenatis egestas. Praesent quis massa vel justo vehicula varius ut pretium turpis. Nullam vel libero a ex egestas scelerisque ac ac nulla. Mauris eget lacinia velit, sit amet rhoncus nibh. Vestibulum rutrum et mauris eget sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse at arcu id purus lacinia tincidunt. Suspendisse non lobortis neque. Sed tincidunt eros ac augue elementum, vitae fringilla urna aliquet.',
     id: 11,
     type: 'remote location'
-}]
+}, {
+    image: '/assets/image3.jpg',
+    name: 'Apartment Complex',
+    description: 'Vestibulum lectus dolor, pellentesque sed blandit sed, tempor eget odio. Maecenas luctus mauris justo, eget fringilla justo fermentum aliquet. Aliquam imperdiet dapibus tellus sit amet ultricies. Duis tincidunt nibh quis lectus dapibus interdum. Duis a viverra tortor. Ut laoreet, justo sit amet posuere tincidunt, ipsum enim dictum quam, quis pulvinar turpis lorem sed urna. Morbi eu mauris sit amet sem aliquam venenatis in ac sapien. Donec sed iaculis nibh. Vestibulum ultricies sapien eget sem placerat, eget porta lacus vehicula. Etiam venenatis massa dignissim velit iaculis, in elementum quam dapibus. Suspendisse semper vulputate elit et imperdiet.',
+    id: 12,
+    type: 'residentual buildings'
+}, {
+    image: '/assets/image5.jpg',
+    name: 'Port Of Somewhere',
+    description: 'Integer egestas mi in arcu elementum, pharetra suscipit risus efficitur. Cras a vehicula dui, quis ultrices justo. Aliquam mattis sem euismod, mattis tellus nec, pretium lorem. Aliquam dignissim urna et auctor sollicitudin. Quisque facilisis placerat lectus eget sodales. Mauris condimentum tempus interdum. Quisque dolor nunc, bibendum ut diam ac, elementum tristique sapien. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Morbi finibus eget nisi nec blandit. Suspendisse purus magna, ornare non magna in, feugiat iaculis justo. Ut non augue erat. Nullam lorem urna, posuere vitae neque quis, vestibulum congue magna. Mauris et tincidunt elit.',
+    id: 13,
+    type: 'Heavy Equipment Installation'
+}, {
+    image: '/assets/image6.jpg',
+    name: 'Power Lines',
+    description: 'Maecenas feugiat nisl non pharetra tempor. Pellentesque porttitor pulvinar dui, nec euismod sem facilisis sed. Sed efficitur faucibus pulvinar. Sed eu tortor eget odio venenatis egestas. Praesent quis massa vel justo vehicula varius ut pretium turpis. Nullam vel libero a ex egestas scelerisque ac ac nulla. Mauris eget lacinia velit, sit amet rhoncus nibh. Vestibulum rutrum et mauris eget sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse at arcu id purus lacinia tincidunt. Suspendisse non lobortis neque. Sed tincidunt eros ac augue elementum, vitae fringilla urna aliquet.',
+    id: 14,
+    type: 'remote location'
+},]
 
 let colors = ['#76ff03', '#1DE9B6', '#FF1744',
+    '#00E5FF', '#C6FF00', '#FFC400',
+    '#FF9100', '#FF3d00', '#F50057','#76ff03', '#1DE9B6', '#FF1744',
     '#00E5FF', '#C6FF00', '#FFC400',
     '#FF9100', '#FF3d00', '#F50057']
 
